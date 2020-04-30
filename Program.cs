c9ce50c58f287a6b4985444e420b3b0dcdb7cd25
Adam Akkari <adam.akkari@epitech.eu>
2020-04-30 19:43:29 +0200

    Created visual studio solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EZChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
